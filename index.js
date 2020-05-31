4173a1a05795a75390f32b7d0b54fcef6a5e6abd
viluzi <47392011+viluzi@users.noreply.github.com>
2020-05-31 11:41:39 +0300

    Added electronics management but commented out, due to developement on windows.

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ var player = require('play-sound')()
 //var FRWheel = new Gpio(5,"out");
 //var RLWheel = new Gpio(6,"out");
 //var RRWheel = new Gpio(7,"out");
+//RRWheel.writeSync(0)
+//RLWheel.writeSync(0)
+//FRWheel.writeSync(0)
+//FLWheel.writeSync(0)
+//forwarding = false
+//righting = false
+//lefting = false
 app.listen(8080);
 
 function handler (req, res) {
@@ -28,18 +35,42 @@ io.on('connection', (socket) => {
     console.log("A controller disconnected!")
   }),
   socket.on("horn", (data) => {
+    console.log("horn")
     player.play('./toot.mp3', (err) => {
       if (err) console.log(`Could not play sound: ${err}`);
   });
  }),
   socket.on("left", (data) => {
     console.log("left")
+    // if (!lefting) {
+    //lefting = true
+    //FRWheel.writeSync(1)
+    //RRWheel.writeSync(1)
+    //} else {lefting = false;FRWheel.writeSync(0);RRWheel.writeSync(0);};
   }),
   socket.on("right", (data) => {
     console.log("right")
+    // if (!righting) {
+    //righting = true
+    //FLWheel.writeSync(1)
+    //RLWheel.writeSync(1)
+    //} else {righting = false;FLWheel.writeSync(0);RLWheel.writeSync(0);};
   }),
   socket.on("forward", (data) => {
     console.log("forward")
+    //if (!forwarding) {
+    //RRWheel.writeSync(1)
+    //RLWheel.writeSync(1)
+    //FRWheel.writeSync(1)
+    //FLWheel.writeSync(1)
+    //forwarding = true
+    //} else {
+    //RRWheel.writeSync(0)
+    //RLWheel.writeSync(0)
+    //FRWheel.writeSync(0)
+    //FLWheel.writeSync(0)
+    //forwarding = false
+    //}
   });
   
   
